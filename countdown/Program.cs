d0c6b06035064aaf114dac2da8306d65594805b3
chajus1 <chajus1@googlemail.com>
2023-07-21 21:56:20 +0100

    Add project files.

---
+++ countdown/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+class CountdownTimer
+{
+    static int targetTime = 5000; // Change this value to set the countdown duration in milliseconds
+    static bool isRunning = true;
+
+    static void Main()
+    {
+        Console.WriteLine("Countdown Timer (in milliseconds)");
+
+        // Start the timer in a separate thread
+        Thread timerThread = new Thread(TimerLoop);
+        timerThread.Start();
+
+        // Wait for the timer to finish
+        timerThread.Join();
+
+        Console.WriteLine("Countdown finished!");
+    }
+
+    static void TimerLoop()
+    {
+        DateTime startTime = DateTime.Now;
+        int elapsedTime;
+
+        while (isRunning)
+        {
+            elapsedTime = (int)(DateTime.Now - startTime).TotalMilliseconds;
+
+            // Calculate remaining time
+            int remainingTime = targetTime - elapsedTime;
+
+            if (remainingTime <= 0)
+            {
+                isRunning = false;
+                break;
+            }
+
+            // Update the display every millisecond
+            Console.WriteLine($"Time remaining: {remainingTime} ms");
+
+            // Sleep for 1 millisecond before updating again
+            Thread.Sleep(1);
+        }
+    }
+}
